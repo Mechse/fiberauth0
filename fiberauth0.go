71b87a1cdf24bfb149c907b1d58da00167670256
Maximilian Leodolter <max.leo2000@gmail.com>
2020-07-21 20:17:09 +0200

    prototype

--- fiberauth0.go
+++ fiberauth0.go
@@ -1,20 +1,55 @@
 package fiberauth0
 
 import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber"
 )
 
-func Extractor() {}
-
 func parseOptions() {}
 
-func checkJWT() bool {
-	return true
+func validationKeyGetter(token *jwt.Token) (interface{}, error) {
+	return []byte("My Secret"), nil
+}
+
+func extractor(c *fiber.Ctx) (string, error) {
+	authHeader := c.Get("Authorization")
+	if authHeader == "" {
+		return "", nil
+	}
+	authHeaderParts := strings.Split(authHeader, " ")
+	if len(authHeaderParts) != 2 || strings.ToLower(authHeaderParts[0]) != "bearer" {
+		return "", errors.New("Authorization header format must be Bearer {token}")
+	}
+	return authHeaderParts[1], nil
+}
+
+func checkJWT(c *fiber.Ctx) bool {
+	token, err := extractor(c)
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		return false
+	}
+
+	parsedToken, err := jwt.Parse(token, validationKeyGetter)
+	if err != nil {
+		fmt.Printf("Error parsing token: %v", err)
+		return false
+	}
+
+	return parsedToken.Valid
 }
 
 // Protected does check your JWT token and validates it
 func Protected() func(*fiber.Ctx) {
 	return func(c *fiber.Ctx) {
-		c.Send("Hello World")
+		if checkJWT(c) {
+			c.Next()
+		} else {
+			c.Send("This route is protected.")
+		}
 	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Mechse/fiberauth0
 
 go 1.14
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gofiber/fiber v1.13.1
+)
